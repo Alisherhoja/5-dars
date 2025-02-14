4faa7c7a654b8ccc0fc46db5c3ef2c173c8cccb8
Alisherhoja <hakeralisher1999@gmail.com>
2025-02-14 22:58:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// 1)Lenght
+// 2)_index
+// 3)incluldes
+// trim probeli yoqatadi
+
+const oquvchi = {
+    ism:"Alisher",
+    yosh:12 ,
+    coin: 1100,
+    group:1746,
+    uylangan:true,
+    bolasi:false,
+}
+
+ console.log(oquvchi)
+
+ const userName = prompt("Ismgizni kiriting");
+ const userSurname = prompt("Familya kiriting");
+ const userAge = +prompt("Yosh kiriting");
+ const usercoin = +prompt("Coin kiriting");
+ const userlogin = prompt("login kiriting");
+ const userpassword = +prompt("parol kiriting");
+ const userteacher = prompt("Ustoz kiriting");
+ const userHp = +prompt("Hp kiriting");
+ const userGroup = prompt("Group kiriting");
+
+
+// alert(oquvchi)
